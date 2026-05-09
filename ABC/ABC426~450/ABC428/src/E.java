91d26c69f16f4d41b67131855fab5f0e83d0e956
nsubaru11 <20051126subaru@gmail.com>
2026-05-09 16:50:55 +0900

    Solved: ABC428 [A ~ E]

--- ABC/ABC426~450/ABC428/src/E.java
+++ ABC/ABC426~450/ABC428/src/E.java
@@ -9,7 +9,7 @@ import java.nio.charset.*;
 import java.util.*;
 import java.util.function.*;
 
-public final class B {
+public final class E {
 
 	// region < Constants & Globals >
 	private static final boolean DEBUG = true;
@@ -24,25 +24,41 @@ public final class B {
 
 	private static void solve() {
 		int n = sc.nextInt();
-		int k = sc.nextInt();
-		String s = sc.next();
-		int max = 0;
-		TreeSet<String> set = new TreeSet<>();
-		for (int i = 0; i < n - k + 1; i++) {
-			String t = s.substring(i, i + k);
-			int cnt = 0;
-			for (int j = 0; j < n - k + 1; j++) {
-				if (s.substring(j, j + k).equals(t)) cnt++;
-			}
-			if (max < cnt) {
-				set.clear();
-				set.add(t);
-				max = cnt;
-			} else if (max == cnt) {
-				set.add(t);
+		Tree tree = new Tree(n);
+		tree.addAll(n - 1, sc::nextInt0, sc::nextInt0);
+		int[] q = new int[n];
+		int[] dist1 = new int[n];
+		int[] dist2 = new int[n];
+		int u = bfs(0, tree, dist1, q);
+		int v = bfs(u, tree, dist1, q);
+		bfs(v, tree, dist2, q);
+		int mx = max(u, v) + 1;
+		for (int i = 0; i < n; i++) {
+			if (dist1[i] > dist2[i]) {
+				out.println(u + 1);
+			} else if (dist1[i] == dist2[i]) {
+				out.println(mx);
+			} else {
+				out.println(v + 1);
 			}
 		}
-		out.println(max).print(set).println();
+	}
+
+	private static int bfs(int u, Tree tree, int[] dist, int[] q) {
+		fill(dist, -1);
+		dist[u] = 0;
+		q[0] = u;
+		int mx = u;
+		for (int head = 0, tail = 1, n = tree.n(); tail < n; head++) {
+			u = q[head];
+			for (final int v : tree.adj(u)) {
+				if (dist[v] != -1) continue;
+				dist[v] = dist[u] + 1;
+				q[tail++] = v;
+				if (dist[mx] < dist[v] || (dist[mx] == dist[v] && mx < v)) mx = v;
+			}
+		}
+		return mx;
 	}
 
 	// region < Utility Methods >
@@ -335,6 +351,147 @@ public final class B {
 			out.flush();
 			System.err.println(deepToString(args));
 		}
+	}
+
+	@SuppressWarnings("unused")
+	private static final class Tree {
+		private final int[] dest, next, first, degree;
+		private final long[] cost;
+		private final int n;
+		private int edgeCount = 0;
+
+		public Tree(final int n) {
+			this.n = n;
+			int m = (n - 1) << 1;
+			dest = new int[m];
+			next = new int[m];
+			first = new int[n];
+			fill(first, -1);
+			degree = new int[n];
+			cost = new long[m];
+		}
+
+		public int n() {
+			return n;
+		}
+
+		public void add(final int i, final int j) {
+			add(i, j, 1);
+		}
+
+		public void add(final int i, final int j, final long c) {
+			dest[edgeCount] = j;
+			next[edgeCount] = first[i];
+			cost[edgeCount] = c;
+			first[i] = edgeCount++;
+			degree[i]++;
+
+			dest[edgeCount] = i;
+			next[edgeCount] = first[j];
+			cost[edgeCount] = c;
+			first[j] = edgeCount++;
+			degree[j]++;
+		}
+
+		public void addAll(int m, final IntSupplier u, final IntSupplier v) {
+			while (m-- > 0) add(u.getAsInt(), v.getAsInt());
+		}
+
+		public void addAll(int m, final IntSupplier u, final IntSupplier v, final LongSupplier cost) {
+			while (m-- > 0) add(u.getAsInt(), v.getAsInt(), cost.getAsLong());
+		}
+
+		public int degree(final int i) {
+			return degree[i];
+		}
+
+		public int to(final int u, final int e) {
+			final int v1 = dest[e << 1];
+			final int v2 = dest[e << 1 | 1];
+			return u != v1 ? v1 : v2;
+		}
+
+		public long cost(final int e) {
+			return cost[e << 1];
+		}
+
+		public int[] adj(final int u) {
+			final int[] adj = new int[degree[u]];
+			for (int e = first[u], i = 0; e != -1; e = next[e], i++) adj[i] = dest[e];
+			return adj;
+		}
+
+		public int[] adjEdgeIds(final int u) {
+			final int[] ids = new int[degree[u]];
+			for (int e = first[u], i = 0; e != -1; e = next[e], i++) {
+				ids[i] = e >> 1;
+			}
+			return ids;
+		}
+
+		public int diameter() {
+			final int[] qV = new int[n], qL = new int[n], qF = new int[n];
+			for (int head = 0, tail = 1; tail < n; head++) {
+				final int u = qV[head], nl = qL[head] + 1, from = qF[head];
+				for (int e = first[u]; e != -1; e = next[e]) {
+					final int v = dest[e];
+					if (from == v) continue;
+					qV[tail] = v;
+					qL[tail] = nl;
+					qF[tail++] = u;
+				}
+			}
+			qV[0] = qV[n - 1];
+			qL[0] = 0;
+			qF[0] = -1;
+			for (int head = 0, tail = 1; tail < n; head++) {
+				final int u = qV[head], nl = qL[head] + 1, from = qF[head];
+				for (int e = first[u]; e != -1; e = next[e]) {
+					final int v = dest[e];
+					if (from == v) continue;
+					qV[tail] = v;
+					qL[tail] = nl;
+					qF[tail++] = u;
+				}
+			}
+			return qL[n - 1];
+		}
+
+		public long diameterCost() {
+			final int[] qV = new int[n], qF = new int[n];
+			final long[] qL = new long[n];
+			int mx = 0;
+			for (int head = 0, tail = 1; tail < n; head++) {
+				final int u = qV[head], from = qF[head];
+				final long l = qL[head];
+				for (int e = first[u]; e != -1; e = next[e]) {
+					final int v = dest[e];
+					if (from == v) continue;
+					qV[tail] = v;
+					qL[tail] = l + cost[e];
+					if (qL[tail] > qL[mx]) mx = tail;
+					qF[tail++] = u;
+				}
+			}
+			qV[0] = qV[mx];
+			qL[0] = 0;
+			qF[0] = -1;
+			mx = 0;
+			for (int head = 0, tail = 1; tail < n; head++) {
+				final int u = qV[head], from = qF[head];
+				final long l = qL[head];
+				for (int e = first[u]; e != -1; e = next[e]) {
+					final int v = dest[e];
+					if (from == v) continue;
+					qV[tail] = v;
+					qL[tail] = l + cost[e];
+					if (qL[tail] > qL[mx]) mx = tail;
+					qF[tail++] = u;
+				}
+			}
+			return qL[mx];
+		}
+
 	}
 
 	@SuppressWarnings("unused")
